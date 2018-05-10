60e2c7a04782900dd035ebcb6661ba501eb86d43
mihail <misha.kukuev@gmail.com>
2018-05-11 00:33:27 +0300

    Corrected task_def source

--- task_def.c
+++ task_def.c
@@ -1,34 +1,70 @@
-#ifndef _TASK_DEF_H
-
-#define _TASK_DEF_H
+#include "task_def.h"
 
 
-#include <stdio.h>
-#include <math.h>
+//--------------------//
+//    Test solution   //
+//--------------------//
+
+double u ( double x1, double x2, double t )
+{
+    return exp ( x1 + x2 + t );
+}
 
 
-#define L1 1
-#define L2 1
-#define T  1
 
 
-// Test solution
-double u ( double x1, double x2, double t );
+//-------------------//
+//     Conditions    //
+//-------------------//
+
+double u0 ( double x1, double x2 ) // t = 0
+{
+    return u ( x1, x2, 0 );
+}
 
 
-// Conditions
-double u0 ( double x1, double x2 ); // t = 0
-
-double m1 ( double x2, double t );  // x1 = 0
-double m2 ( double x2, double t );  // x1 = L1
-double m3 ( double x1, double t );  // x2 = 0
-double m4 ( double x1, double t );  // x2 = L2
+double m1 ( double x2, double t )  // x1 = 0
+{
+    return u ( 0, x2, t );
+}
 
 
-// Coefficients
-double k1 ( double x1, double x2, double t );
-double k2 ( double x1, double x2, double t );
+double m2 ( double x2, double t )  // x1 = L1
+{
+    return u ( ( double ) L1, x2, t );
+}
 
-double f ( double x1, double x2, double t );
 
-#endif
+double m3 ( double x1, double t )  // x2 = 0
+{
+    return u ( x1, 0, t );
+}
+
+
+double m4 ( double x1, double t )  // x2 = L2
+{
+    return u ( x1, ( double ) L2, t );
+}
+
+
+
+//---------------------//
+//     Coefficients    //
+//---------------------//
+
+double k1 ( double x1, double x2, double t )
+{
+    return exp ( -( x1 + x2 + t ) );
+}
+
+
+double k2 ( double x1, double x2, double t )
+{
+    return exp ( -( x1 + x2 + t ) );
+}
+
+
+double f ( double x1, double x2, double t )
+{
+    return exp ( x1 + x2 + t );
+}
